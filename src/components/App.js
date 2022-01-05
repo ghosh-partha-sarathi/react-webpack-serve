e4d504b9035c52f9930dc1d85e63abd6e93d555d
ghosh-partha-sarathi <partha187@gmail.com>
2022-01-05 14:22:25 +0000

    initial commit for the project code

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => (
+    <div>
+        <p><h2>Hello World</h2></p>
+    </div>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports={
+    entry: "./src/index.js",
+    mode: "development",
+    output: {
+        filename: "bundle.[hash].js",
+        path: path.resolve(__dirname, "dist")
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+        }),
+    ],
+    resolve: {
+        modules: [__dirname, "src", "node_modules"],
+        extensions: ["*", ".js", ".jsx", ".ts", ".tsx"],
+    },
+    module: {
+        rules: [ 
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    }
+}
