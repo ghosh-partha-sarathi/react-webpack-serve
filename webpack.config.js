db7c065b5ae7dfb22b68bdc5331343b1b88fe68e
ghosh-partha-sarathi <partha187@gmail.com>
2022-01-07 18:41:03 +0000

    webpack config and setup info updated to allow external accessibilty to application

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,36 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
-module.exports={
-    entry: "./src/index.js",
-    mode: "development",
-    output: {
-        filename: "bundle.[hash].js",
-        path: path.resolve(__dirname, "dist")
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index.html",
-        }),
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    filename: "bundle.[hash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    allowedHosts: "all",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  resolve: {
+    modules: [__dirname, "src", "node_modules"],
+    extensions: ["*", ".js", ".jsx", ".ts", ".tsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
     ],
-    resolve: {
-        modules: [__dirname, "src", "node_modules"],
-        extensions: ["*", ".js", ".jsx", ".ts", ".tsx"],
-    },
-    module: {
-        rules: [ 
-            {
-                test: /\.js?$/,
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
-            }
-        ]
-    }
-}
+  },
+};
